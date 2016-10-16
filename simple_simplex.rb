9946e4ea72df0ee7729f849f366c37f0a1be060a
Eric <eric.talle@hp.com>
2016-10-16 14:45:13 -0700

    calculating the current basis.  Put printing output to one method.  solve now returns a hash of solution data, z, steps to optimal and basis.

--- simple_simplex.rb
+++ simple_simplex.rb
@@ -4,64 +4,46 @@
 #a bit about Ruby and Linear Programming.  Hopefully it can be used as a learning 
 #tool for the interested.
 #
+require 'matrix'
 
 class SimpleSimplex
 	attr_accessor :show_tableau
 	
 	def initialize(input)
 		@show_tableau = false
+		@step = 0
 		if input.kind_of?(Array)
 			@lp = input
-		elsif input.kind_of?(String)  #file name or url
-			raise "Input is a String!  Nothing implemented yet for file of input."
+		else
+			raise "Input is a String!  Nothing implemented yet for anyother input."
 		end
-	end
-
-	def raw_tableau
-		return @lp
 	end
 
 	def solve
+		solution_hash = {}
 		if @show_tableau
-			puts "Initial tableau..."
 			print_tableau
 		end
 		while !optimal?
+			@step += 1
 			pivot
 		end
-		if @show_tableau
-			puts ">>>>>> z = #{print_rational(current_z_value)}"
-		end
-		current_z_value
+		solution_hash[:max_z] = current_z_value.to_f
+		solution_hash[:step] = @step.to_i
+		solution_hash[:basis] = current_basis
+		return solution_hash
 	end
 
 	def current_rhs
 		@lp[0..-3].collect{|row| row[-1]}
 	end	
 
-	def print_tableau
-		@lp[-1].each {|e| print "#{e}\t"}
-		puts ""
-		puts "-----------------------------------------------------"
-		@lp[0..-3].each do |row|
-			row.each {|e| print "#{print_rational(e)}\t"}
-			puts ""		
-		end	
-		puts "-----------------------------------------------------"
-		@lp[-2].each {|e| print "#{print_rational(e)}\t"}
-		puts "\n\n"
-	end
-
 	def pivot
 		pi = pivot_index
 		pivot_row_index = pi[0]
 		pivot_column_index = pi[1]
 		pivot_scaler = @lp[pivot_row_index][pivot_column_index]	
-		if @show_tableau
-			puts "\npivot: #{pivot_scaler} @ location: (#{pivot_row_index},#{pivot_column_index})" 
-		end
 		pivot_row = @lp[pivot_row_index].collect! {|n| n.to_r/pivot_scaler.to_r}
-
 		@lp[0..-2].each_with_index do |row,index|
 			if index != pivot_row_index
 				scaler = @lp[index][pivot_column_index]	* -1
@@ -73,7 +55,7 @@ class SimpleSimplex
 		end
 	end
 
-	def print_rational(rational_number)
+	def make_number_real(rational_number)
 		rational_number.to_r
 		if rational_number.numerator == 0
 			return 0
@@ -88,13 +70,34 @@ class SimpleSimplex
 		@lp[-2].min < 0 ? false : true
 	end
 	
-	def ero(scaler, pivot_row, target_row_index)
-		operand_array = [pivot_row.collect {|n| n*scaler}, @lp[target_row_index]]
-		@lp[target_row_index] = operand_array.transpose.map {|e| e.reduce(:+).to_r}
-	end
-
 	def current_z_value
 		return @lp[-2][-1]
+	end
+
+	def current_basis
+		basis = []
+		transpose = []
+		m = []
+		@lp.each do |r|
+			m = Matrix.rows(m.to_a << r)
+		end
+		transpose = m.transpose.to_a
+
+		transpose.each do |col|
+			if column_basic?(col)
+				basis << col[-1]
+			end
+		end
+		return basis	
+	end
+
+	def column_basic?(ary)
+		if(ary[0..-2].count(0) == ary.length - 2)
+			unless ary.index(1).nil?
+				return true
+			end
+		end
+		return false
 	end
 
 	def pivot_index
@@ -106,17 +109,37 @@ class SimpleSimplex
 		end
 		return pr_index, pc_index
 	end
+
+	def ero(scaler, pivot_row, target_row_index)
+		operand_array = [pivot_row.collect {|n| n*scaler}, @lp[target_row_index]]
+		@lp[target_row_index] = operand_array.transpose.map {|e| e.reduce(:+).to_r}
+	end
+
+	def print_tableau
+		@lp[-1].each {|e| print "#{e}\t"}
+		puts ""
+		puts "-----------------------------------------------------"
+		@lp[0..-3].each do |row|
+			row.each {|e| print "#{make_number_real(e)}\t"}
+			puts ""		
+		end	
+		puts "-----------------------------------------------------"
+		@lp[-2].each {|e| print "#{make_number_real(e)}\t"}
+		puts "\n\n"
+	end
 end
 
-#example usage
-# lp = SimpleSimplex.new([[1,2,1,0,0,0,16],
-# 												[1,1,0,1,0,0,9],
-# 												[3,2,0,0,1,0,24],
-# 												[-40,-30,0,0,0,1,0],
-# 												["x1","x2","s1","s2","s3","z","rhs"]])
-# lp.show_tableau = true
-# puts "#{lp.current_rhs}"
-# lp.solve
-# use rspec spec to run tests
 
 __END__
+#example usage
+lp = SimpleSimplex.new([[1,2,1,0,0,0,16],
+												[1,1,0,1,0,0,9],
+												[3,2,0,0,1,0,24],
+												[-40,-30,0,0,0,1,0],
+												["x1","x2","s1","s2","s3","z","rhs"]])
+lp.show_tableau = true
+solution = lp.solve
+puts "solution: #{solution}"
+
+# use rspec spec to run tests
+
